46c19d8400fdab69855d9bd0a0f3970e8bb3f8bd
VasiHemanth <hemanth.vasi1716@gmail.com>
2021-06-17 22:29:55 +0530

    Update rest_config.js

--- src/config/rest_config.js
+++ src/config/rest_config.js
@@ -1,7 +1,7 @@
 export const article_url = 'https://newsapi.org/v2/top-headlines';
 export const country_code = 'in';
 export const category = 'business';
-export const _api_key = '412e0d03427747dfb9b328360039e05e';
+export const _api_key = 'Enter_your_api_key';
 
 
 
